6140e5c6af090c2b552c34c9535702105b38e751
jared <jcraig1730@gmail.com>
2019-09-19 15:34:06 -0700

    refactor db connection to use pool instead of client; cleaned typos in cassandraSchema

--- database/controllers.js
+++ database/controllers.js
@@ -1,23 +1,23 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const config = require('./config.js')
-const client = new Client(config);
+const pool = new Pool(config);
 
-client.connect();
+pool.connect();
 
 const getListingDataByListingId = (req, res, id) => {
-  client.query(`SELECT * FROM listing where id=${id}`)
+  pool.query(`SELECT * FROM listing where id=${id}`)
     .then(data => res.status(200).json(data.rows))
     .catch(err => res.status(400).json({ err }))
 }
 
 const getReservationsByListingId = (req, res, listingid) => {
-  client.query(`SELECT id, startDate, endDate, numGuests, numInfants, listingID FROM reserveddates where listingid=${listingid}`)
+  pool.query(`SELECT id, startDate, endDate, numGuests, numInfants, listingID FROM reserveddates where listingid=${listingid}`)
     .then(data => res.status(200).json(data.rows))
     .catch(err => res.status(400).end())
 }
 
 const getReservationByReservationId = (req, res, reservationid) => {
-  client.query(`SELECT * FROM reserveddates where id = ${reservationid}`)
+  pool.query(`SELECT * FROM reserveddates where id = ${reservationid}`)
     .then(data => res.status(200).json(data.rows))
     .catch(err => res.status(404).end())
 }
@@ -27,7 +27,7 @@ const createReservation = (req, res, listingid, reservationData) => {
     text: 'INSERT INTO reserveddates (startDate, endDate, numGuests, numInfants, listingid) VALUES($1, $2, $3, $4, $5)',
     values: [reservationData.startDate, reservationData.endDate, reservationData.numGuests, reservationData.numInfants, listingid]
   }
-  client.query(query)
+  pool.query(query)
     .then(data => res.status(201).end())
     .catch(err => res.status(400).end())
 }
@@ -38,18 +38,22 @@ const updateReservation = (req, res, reservationid, reservationData) => {
   Object.keys(reservationData).forEach(item => {
     set.push(`${item} = ($${counter += 1})`)
   })
+
   const query = {
     text: `UPDATE reserveddates SET ${set} WHERE id = ($${counter += 1})`,
     values: Object.values(reservationData).concat(Number(reservationid))
   }
 
-  client.query(query)
+  console.log(query.text)
+
+
+  pool.query(query)
     .then((data) => res.status(201).json(data))
     .catch((err) => res.status(404).json(err))
 }
 
 const deleteReservation = (req, res, reservationid) => {
-  client.query(`DELETE FROM reserveddates WHERE id = ${reservationid}`)
+  pool.query(`DELETE FROM reserveddates WHERE id = ${reservationid}`)
     .then(() => res.status(200).end())
     .catch(err => res.status(404).end())
 }
--- database/seedPostgres.sql
+++ database/seedPostgres.sql
@@ -1,3 +1,3 @@
-COPY listing(id, perNight, rating, ratingAmount, guestsAllowed, guestsInfants, cleaningFee, serviceFee, occupancyFee, daysMinimum) FROM '/home/jared/hackreactor/sdc/reservation-module/database/listing.csv' delimiter ',' csv header;
+COPY listing(id, perNight, rating, ratingAmount, guestsAllowed, guestsInfants, cleaningFee, serviceFee, occupancyFee, daysMinimum) FROM '/home/jared/hackreactor/sdc/reservation-module/database/listing.csv' DELIMITER ',' CSV HEADER;
 
-copy reserveddates(id, startDate, endDate, numGuests, numInfants, listingID) FROM '/home/jared/hackreactor/sdc/reservation-module/database/reservations.csv' delimiter ',' csv header;
+COPY reserveddates(id, startDate, endDate, numGuests, numInfants, listingID) FROM '/home/jared/hackreactor/sdc/reservation-module/database/reservations.csv' DELIMITER ',' CSV HEADER;
